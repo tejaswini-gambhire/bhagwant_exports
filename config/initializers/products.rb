050c85bede27e616c69e25ffbd0526aa37177b69
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2018-07-22 15:32:11 +0530

    load products dynamically

--- config/initializers/products.rb
+++ config/initializers/products.rb
@@ -7,23 +7,37 @@ PRODUCTS= [
     display: true
   },
   {
-    id: "custard-apple",
-    title: "Custard Apple",
-    image: "custard-apple.jpg",
+    id: "pomegranate",
+    title: "Pomegranate",
+    image: "pomegranate.jpeg",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
     display: true
   },
   {
-    id: "custard-apple",
-    title: "Custard Apple",
-    image: "custard-apple.jpg",
+    id: "onion",
+    title: "Onion",
+    image: "onion.png",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
     display: true
   },
   {
-    id: "custard-apple",
-    title: "Custard Apple",
-    image: "custard-apple.jpg",
+    id: "drumstick",
+    title: "Drumstick",
+    image: "drumstick.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+    display: true
+  },
+  {
+    id: "drumstick-seeds",
+    title: "Drumstick Seeds",
+    image: "drumstick-seeds.jpg",
+    description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
+    display: true
+  },
+  {
+    id: "tamarind-kernel-powder",
+    title: "Tamarind Kernel Powder",
+    image: "tamarind-kernel-powder.jpg",
     description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
     display: true
   }
