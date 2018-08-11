aa062d5d9bfd9f872a21e58829c72054c164b4ae
tejaswini-gambhire <gambhire.tejaswini30@gmail.com>
2018-08-12 00:16:42 +0530

    modified production.rb file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -88,4 +88,18 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { protocol: 'http', host: 'www.bhagwantexport.com' }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    authentication: "plain",
+    user_name: ENV['USERNAME'],
+    password: ENV['PASSWORD'],
+    enable_starttls_auto: true
+  }
 end
